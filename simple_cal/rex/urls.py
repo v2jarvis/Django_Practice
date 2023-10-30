df156d386aadb5f888a35bdaf9ca365b957706e9
v2jarvis <bittoojarvis@gmail.com>
2023-10-31 00:02:59 +0530

    add templates dir and some config

--- simple_cal/rex/urls.py
+++ simple_cal/rex/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.dis,name="dis")
+    path('',views.dis,name="dis"),
+    path('home',views.read,name="home")
 ]
--- simple_cal/rex/views.py
+++ simple_cal/rex/views.py
@@ -1,5 +1,8 @@
+#all views here
 from django.shortcuts import render
 from django.http import HttpResponse
+
 def dis(request):
     return HttpResponse("helloo this is simple calculator")
-# Create your views here.
+def read(request):
+    return render(request,'index.html')
