49379511dfa7a60e8999fd5c0b3555e5afdf6903
v2jarvis <bittoojarvis@gmail.com>
2023-11-09 22:48:40 +0530

    Registration and Login Authentication using Django

--- login_page/myapp/urls.py
+++ login_page/myapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.home,name="home"),
-    path('register',views.register,name="register"),
-    path('login',views.loginn,name="login")
+    path('register/',views.register,name="register"),
+    path('login/',views.loginn,name="login")
     
 ]
