375c4b56607303d6a5523c657176e03ef2c21fd8
v2jarvis <bittoojarvis@gmail.com>
2023-11-09 22:11:44 +0530

    Registration and Login Authentication using Django

---
+++ login_page/myapp/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('',views.home,name="home"),
+    path('register',views.register,name="register"),
+    path('login',views.loginn,name="login")
+    
+]
---
+++ login_page/myapp/views.py
@@ -0,0 +1,41 @@
+# Create your views here.
+#import lib
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth import authenticate
+
+def home(request):
+    return render(request,"home.html")
+
+def register(request):
+    if request.method=='POST':
+        user=request.POST['user']
+        email=request.POST['email']
+        cpass=request.POST['cpass']
+        rpass=request.POST['rpass']
+
+        if(cpass==rpass):
+            msg=User.objects.filter(username=user).exists()
+            if(msg):
+                print("Already Exist")
+                return redirect('register')
+            else:
+                User.objects.create(username=user,email=email,password=make_password(rpass))
+                return redirect('login')
+        else:
+            return HttpResponse("<script>alert('Password Not Match');</script>")
+    else:
+        return render(request,'register.html') 
+
+def loginn(request):
+    if request.method=='POST':
+        user=request.POST['user']
+        password=request.POST['password']
+        user=authenticate(username=user,password=password)
+        if(user is not None):
+            return HttpResponse(user)
+        else:
+            return HttpResponse("<script>alert('Password Not Match');</script>")
+    return render(request,'login.html')            
