6aab6cbd10709a7184b8306cbec035dabf3e2ef4
v2jarvis <bittoojarvis@gmail.com>
2023-11-17 23:56:00 +0530

    add the mysql database

--- form_crud/form_crud/settings.py
+++ form_crud/form_crud/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'form_crud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crud_data',
+        'USER':'root',
+        'PASSWORD':'data@123',
+        'PORT':3306,
+        'HOST':'localhost'
     }
 }
 
