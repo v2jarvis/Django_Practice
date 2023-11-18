c3c7b45c86b1c87e52e5c2d896645c2ddfd552a9
v2jarvis <bittoojarvis@gmail.com>
2023-11-18 22:41:23 +0530

    create model like name,email,mobile

--- form_crud/mycrud/models.py
+++ form_crud/mycrud/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class info(models.Model):
+    name=models.CharField(max_length=20)
+    email=models.EmailField()
+    mobile=models.BigIntegerField()
+    
