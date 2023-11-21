e653934b2c2799ba0bd96957b84fced370900760
v2jarvis <bittoojarvis@gmail.com>
2023-11-21 23:53:07 +0530

    modify the urls and views

--- form_crud/mycrud/urls.py
+++ form_crud/mycrud/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.add,name="add"),
-    path('show',views.myshow,name="show")
+    path('show',views.myshow,name="show"),
+    path('<int:id>',views.delete,name="del"),
 ]
--- form_crud/mycrud/views.py
+++ form_crud/mycrud/views.py
@@ -13,7 +13,8 @@ def add(request):
         return redirect('show')
     myobj=crudform()
     return render(request,'add.html',{'add':myobj})
+
 def delete(request,id):
-    data=info.objects.get(id=id)
-    data.delete()
-    
+     data=info.objects.get(id=id)
+     data.delete()
+     return render(request,"show.html")
