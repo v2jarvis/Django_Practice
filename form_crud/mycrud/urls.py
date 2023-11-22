f36406a3835245febff56b2777245870dd25f6f3
v2jarvis <bittoojarvis@gmail.com>
2023-11-23 00:15:09 +0530

    add the edit method in views

--- form_crud/mycrud/urls.py
+++ form_crud/mycrud/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('',views.add,name="add"),
     path('show',views.myshow,name="show"),
-    path('<int:id>',views.delete,name="del"),
-    path('<int:id>',views.edit,name="edit")
+    path('del/<int:id>/',views.delete,name="del"),
+    path('edit/<int:id>/',views.edit,name="edit")
 ]
