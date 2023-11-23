d1d5cd664bda578e5581db1407c4281b60207d5f
v2jarvis <bittoojarvis@gmail.com>
2023-11-23 23:15:33 +0530

    add the cripsy forms for bootstarp

--- form_crud/form_crud/settings.py
+++ form_crud/form_crud/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'mycrud',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -69,7 +72,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'form_crud.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
--- form_crud/mycrud/forms.py
+++ form_crud/mycrud/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 from .models import info
-
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
 
 class crudform(forms.ModelForm):
     class Meta:
         model=info
         fields='__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(crudform, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit', 'Submit'))    
