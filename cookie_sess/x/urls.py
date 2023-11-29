31898771e463bda0a37f78d42cc5dbe4b5ce3f97
v2jarvis <bittoojarvis@gmail.com>
2023-11-30 01:11:09 +0530

    modify the views and make simple session

--- cookie_sess/x/urls.py
+++ cookie_sess/x/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('',views.set_session,name="set"),
+    path('get',views.get_session,name="get")
 ]
--- cookie_sess/x/views.py
+++ cookie_sess/x/views.py
@@ -1,2 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def set_session(request):
+    request.session['user_id'] = 1
+    return HttpResponse("Session set!")
+
+def get_session(request):
+    user_id = request.session.get('user_id', None)
+    return HttpResponse(f"User ID: {user_id}")
 
