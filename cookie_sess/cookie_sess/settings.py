09d34fb49695d4452ad15f95b3c7dde4e96e8ade
v2jarvis <bittoojarvis@gmail.com>
2023-11-30 01:18:28 +0530

    add the sessions

--- cookie_sess/cookie_sess/settings.py
+++ cookie_sess/cookie_sess/settings.py
@@ -76,8 +76,12 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'sess_cooki',
+        'USER': 'root',
+        'PASSWORD': 'data@123',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
