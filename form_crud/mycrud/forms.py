3242af5a82f5c1d82964927c65a9835ae71dba03
v2jarvis <bittoojarvis@gmail.com>
2023-12-01 23:49:57 +0530

    in login add text utils

--- form_crud/mycrud/forms.py
+++ form_crud/mycrud/forms.py
@@ -1,15 +1,12 @@
 from django import forms
 from .models import info
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Submit
 
-class crudform(forms.ModelForm):
-    class Meta:
-        model=info
-        fields='__all__'
+# class crudform(forms.ModelForm):
+#     class Meta:
+#         model=info
+#         fields='__all__'
 
-    def __init__(self, *args, **kwargs):
-        super(crudform, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_method = 'post'
-        self.helper.add_input(Submit('submit', 'Submit'))    
+class crudform(forms.Form):
+    name=forms.CharField(max_length=20)
+    email=forms.EmailField()
+    mobile=forms.IntegerField()
